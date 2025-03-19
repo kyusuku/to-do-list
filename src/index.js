e19a37c2a8dce189e2303ba2a29e8acb66beb382
kyusuku <147250847+kyusuku@users.noreply.github.com>
2025-03-19 18:01:43 +0700

    Set up basic "Todo" class

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import "./styles.css";
+
+class Todo {
+    constructor(title, description, dueDate, priority, notes, checklist) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.notes = notes;
+        this.checklist = checklist;
+    }
+}
